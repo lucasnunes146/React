df95cab08358b1d5a355503e6df201294482ec75
lucasnunes146 <lucasnunes146@gmail.com>
2022-09-20 15:58:41 -0300

    implementando inputs e labels no formulario

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -2,11 +2,13 @@ import './CampoTexto.css'
 
 // outra forma de declarar uma função
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
     return (
-        <div>
-            <label>Nome</label>
-            <input></input>
+        <div className="campo-texto">
+            <label>
+                {props.label}
+            </label>
+            <input placeholder={props.placeholder}/>
         </div>
     )
 }
