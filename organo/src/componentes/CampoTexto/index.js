6a4cab3e237d2d2473e37436844fa2cb2c0abd62
lucasnunes146 <lucasnunes146@gmail.com>
2022-09-20 15:18:54 -0300

    inclusão de formulário

---
+++ organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,14 @@
+import './CampoTexto.css'
+
+// outra forma de declarar uma função
+
+const CampoTexto = () => {
+    return (
+        <div>
+            <label>Nome</label>
+            <input></input>
+        </div>
+    )
+}
+
+export default CampoTexto
