087d1c09077c49965da33590259291c7ec2e9fd1
lucasnunes146 <lucasnunes146@gmail.com>
2022-09-20 14:53:26 -0300

    header do projeto com a inclusão de banner

--- organo/src/Banner/Banner.js
+++
@@ -1,7 +0,0 @@
-function Banner() {
-    return (
-        <img src="/imagens/imagens/banner.png" alt="Banner principal do site"/>
-    )
-}
-
-export default Banner
---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">        
+            <img src="/imagens/imagens/banner.png" alt="Banner principal do site"/>
+        </header>
+    )
+}
+
+export default Banner
