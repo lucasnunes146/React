d2e42698f0a0c944d75d10f719b21a7e427add24
lucasnunes146 <lucasnunes146@gmail.com>
2022-11-04 16:59:17 -0300

    alteração na forma de preenchimento do input

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -3,12 +3,17 @@ import './CampoTexto.css'
 // outra forma de declarar uma função
 
 const CampoTexto = (props) => {
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
